b54c8dfeee80bb1828fc562af51d558961938de8
Bharat-Prajapati <bharatprajapati4248@gmail.com>
2025-05-06 12:43:06 +0530

    Last commit for this app

--- app/src/main/java/com/example/apnaroom/activities/SplashActivity.java
+++ app/src/main/java/com/example/apnaroom/activities/SplashActivity.java
@@ -1,6 +1,12 @@
 package com.example.apnaroom.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,28 +17,59 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.apnaroom.R;
+import com.example.apnaroom.databinding.ActivitySplashBinding;
+import com.example.apnaroom.utills.AndroidUtils;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//            }
+//        }, 3000);
+
+        binding.startBtn.setOnClickListener(v->{
+            if (isNetworkAvailable(SplashActivity.this)) {
                 Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(intent);
-
                 overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
-        }, 3000);
+            else {
+                AndroidUtils.showToast(SplashActivity.this, "Please check network connection");
+            }
+        });
+    }
+
+    private boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(context.CONNECTIVITY_SERVICE);
+
+        if (connectivityManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                Network network = connectivityManager.getActiveNetwork();
+                if (network == null) return false;
+
+                android.net.NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+                return capabilities != null && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+            }else {
+                NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+                return activeNetwork != null && activeNetwork.isConnected();
+            }
+        }
+        return false;
     }
 }
